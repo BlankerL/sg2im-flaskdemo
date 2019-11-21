ef718217d9a27926c583d212c69e27d8f24bfbbc
Isaac Lin <me@isaaclin.cn>
2019-11-21 14:23:09 +0800

    Redesigned addRow.js to make the response to server more structured.

--- sg2im_flask/static/js/addRows.js
+++ sg2im_flask/static/js/addRows.js
@@ -1,11 +1,23 @@
 function addRow(){
     $("#table").each(
         function () {
-            var tds = '<tr>';
-            jQuery.each($('tr:last td', this), function () {
-                tds += '<td>' + $(this).html() + '</td>';
-            });
+            let rowId = Number($('#table tr:last td label').text());
+
+            let tds = '<tr>';
+            tds += '<td>' + '<label id="Document ID">' + (rowId + 1) + '</label>' + '</td>';
+            tds += '<td>' + '<input type="text" name="obj1-' + (rowId + 1) +'"/>' + '</td>';
+            tds += '<td>' + '<select name="rel-' + (rowId + 1) + '">'
+                + '<option value="on">on</option>' +
+                '<option value="in">in</option>' +
+                '<option value="by">by</option>' +
+                '<option value="below">below</option>' +
+                '<option value="above">above</option>' +
+                '<option value="next to">next to</option>' +
+                '<option value="in front of">in front of</option>'
+                + '</select>' + '</td>';
+            tds += '<td>' + '<input type="text" name="obj2-' + (rowId + 1) +'"/>' + '</td>';
             tds += '</tr>';
+
             if ($('tbody', this).length > 0) {
                 $('tbody', this).append(tds);
             } else {
