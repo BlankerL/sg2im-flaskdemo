dae71bf87fe2d6074c3f7da65bcea9113825743c
Isaac Lin <me@isaaclin.cn>
2019-11-21 14:45:56 +0800

    Finish the parser, it can load the data from form and send it to the picture generator.

--- sg2im_flask/api/views.py
+++ sg2im_flask/api/views.py
@@ -22,5 +22,33 @@ def generate():
 
 @api.route('/parser', methods=['POST'])
 def get_image():
-    print(request.form)
-    return 'Hello!'
+    # Initiate a relationship dict to store the posted data.
+    relationship_count = len(request.form) // 3
+    relationship = dict()
+    # Initiate a payload dict to post data to `generate` function.
+    payload = dict()
+    payload['relationships'] = list()
+    payload['objects'] = list()
+    # Initiate a objects set to store all the objects.
+    objects = set()
+
+    for i in range(relationship_count):
+        relationship[i + 1] = (
+            request.form.get('obj1-' + str(i + 1)),
+            request.form.get('rel-' + str(i + 1)),
+            request.form.get('obj2-' + str(i + 1)),
+        )
+        objects.add(request.form.get('obj1-' + str(i + 1)))
+        objects.add(request.form.get('obj2-' + str(i + 1)))
+
+    objects = list(objects)
+    payload['objects'] = objects
+
+    for value in relationship.values():
+        temp = list()
+        temp.append(objects.index(value[0]))
+        temp.append(value[1])
+        temp.append(objects.index(value[2]))
+        payload['relationships'].append(temp)
+
+    return generate(scene_graph=payload)
